dadf3c3995b82e80bf1cd410c47f0c7b6d23ce96
Josue Cardona <JC-Home-Folder@Josues-Air.home>
2015-04-14 18:11:11 -0400

    Initial Commit

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Josue Cardona on 4/14/15.
+//  Copyright (c) 2015 Josué Cardona - test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
